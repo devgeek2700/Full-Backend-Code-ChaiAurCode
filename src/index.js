99052633d3aba44b067dee0736f386c3fad9831c
devgeek <nehasinghpro2700@gmail.com>
2024-06-14 05:35:52 +0530

    utils Code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 300, () => {
+      console.log(`Server listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`MOGODB DB CONNECTION FAILED !!!`, error);
+  });
 
 /*
 import express from "express";
