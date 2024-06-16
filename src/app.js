86aa22951e3c9ac51fb7b21624bf5fa646434dfe
devgeek <nehasinghpro2700@gmail.com>
2024-06-16 13:31:40 +0530

    Controllers Checking in Postman

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
@@ -14,5 +15,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
- 
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes Declaration
+app.use("/api/v1/users", userRouter); // --> http://localhost:3000/api/v1/users/register
+
 export { app };
